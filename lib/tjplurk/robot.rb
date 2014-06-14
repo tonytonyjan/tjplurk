a8bd4a15eb2fb227e71c74299a9ba1874588448a
Tony Jian <tonytonyjan@gmail.com>
2014-06-14 23:08:27 +0800

    don't use bang method

--- lib/tjplurk/robot.rb
+++ lib/tjplurk/robot.rb
@@ -7,7 +7,7 @@ module Tjplurk
     def respond input
       if matched = @topics.each{|topic| topic.pattern = Regexp.new(topic.pattern) if topic.pattern.is_a? String}.select{|topic| topic.pattern.match(input) }.sample
         response = matched.responses.sample.content
-        input.gsub!(matched.pattern, response)
+        input.gsub(matched.pattern, response)
       end
     end
   end
--- lib/tjplurk/version.rb
+++ lib/tjplurk/version.rb
@@ -1,3 +1,3 @@
 module Tjplurk
-  VERSION = '1.4.2'
+  VERSION = '1.4.3'
 end
